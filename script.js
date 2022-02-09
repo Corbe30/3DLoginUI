ed8cb719723cdf856279c12d661c9a7cbee1a0e9
Shashank Agarwal <53386582+Corbe30@users.noreply.github.com>
2022-02-09 10:47:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-import * as THREE from 'https://cdn.skypack.dev/three@0.128.0/build/three.module.js';
-import { OrbitControls } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/controls/OrbitControls.js';
+import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
+
+import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js';
+
 import * as dat from "https://cdn.skypack.dev/dat.gui@0.7.7"
 import { GLTFLoader } from "https://threejs.org/examples/jsm/loaders/GLTFLoader.js"
 
